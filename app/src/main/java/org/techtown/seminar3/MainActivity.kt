e9e4895eb40f11b973fdc5b2e5b69f2d8f40a9f3
murjune <sis9221302@gmail.com>
2022-06-30 20:52:09 +0900

    [FEAT] 앱 내 외부저장소에 캡쳐한 image 저장하는 기능 구현 완료

--- app/src/main/java/org/techtown/seminar3/MainActivity.kt
+++ app/src/main/java/org/techtown/seminar3/MainActivity.kt
@@ -1,61 +1,74 @@
 package org.techtown.seminar3
 
-import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import org.techtown.seminar3.databinding.ActivityMainBinding
-import java.io.FileOutputStream
+import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var photoURI: Uri
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        changeProfileImage()
+        setContentView(binding.root)
+    }
+    /**
+     * 카메라 권한 체크하는 launcher
+     */
     private val cameraPermissionLauncher: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
             var flag = true
+            // 권한 체크
             it.forEach {
                 if (!it.value) {
                     flag = false
                 }
             }
+            // 권한이 허용됐을 경우
             if (flag) {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                intent.resolveActivity(packageManager)
-                cameraLauncher.launch(intent)
+                // 사진을 캡처하는 인텐트를 호출하는 코드
+                photoURI = Uri.EMPTY
+                val fullSizePictureIntent = getPictureIntent(applicationContext).apply {
+                    // applicationContext 대신 this도 OK
+                    resolveActivity(packageManager)
+                }
+                cameraLauncher.launch(fullSizePictureIntent)
             }
         }
     private val galleyPermissionLauncher: ActivityResultLauncher<String> =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            Log.d(TAG, "MainActivity - galleyPermissionLauncher called")
             if (isGranted) {
+                Log.d(TAG, "MainActivity - galleyPermissionLauncher - granted")
                 galleryLauncher.launch("image/*")
             }
         }
 
     private val cameraLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val uri: Uri?
+            Log.d(TAG, "MainActivity - cameraLauncher called")
             if (result.resultCode == RESULT_OK && result.data != null) {
-                val bitmap = (result.data!!.extras?.get("data") as Bitmap?)
-                bitmap.let { curBitmap ->
-                    val fileName = newFileName()
-                    uri = saveImageFile(fileName, "image/jpeg", curBitmap!!)
-                    Log.d(TAG, "MainActivity -  cameraLauncher - uri: $uri")
-                    binding.ivAttached.setImageURI(uri)
-                    return@registerForActivityResult
-                }
-                Log.e(TAG, "MainActivity - cameraLauncher - bitmap: $bitmap")
+                binding.ivAttached.setImageURI(photoURI)
             } else if (result.resultCode == RESULT_OK) {
-                contentResolver.delete(uri!!, null, null)
                 Toast.makeText(this, "사진 선택을 취소하셨습니다.", Toast.LENGTH_SHORT).show()
             } else {
                 Log.d(TAG, "MainActivity - cameraLauncher에서 알 수 없는 오류 발생")
@@ -64,6 +77,7 @@ class MainActivity : AppCompatActivity() {
 
     private val galleryLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            Log.d(TAG, "MainActivity - galleryLauncher called")
             if (uri != null)
                 if (Build.VERSION.SDK_INT < 28) {
                     val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
@@ -77,49 +91,56 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "사진 선택을 취소하셨습니다.", Toast.LENGTH_SHORT).show()
             }
         }
+    /**
+     * 2) 카메라 호출할 Intent 호출
+     */
+    fun getPictureIntent(context: Context): Intent {
+        val fullSizeCaptureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // 1) File 생성 - 촬영 사진이 저장 될
+        // photoFile 경로 = /storage/emulated/0/Android/data/패키지명/files/Pictures/
+        val photoFile: File? = try {
+            createImageFile(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES))
+        } catch (ex: IOException) {
+            // Error occurred while creating the File
+            ex.printStackTrace()
+            null
+        }
+        // 2) 생성된 File로 부터 Uri 생성 (by FileProvider)
+        // URI 형식 EX) content://com.example.img.fileprovider/camera_images/20220630_0811_124213.jpg
+        photoFile?.let {
+            photoURI = FileProvider.getUriForFile(
+                context,
+                BuildConfig.APPLICATION_ID + ".fileprovider",
+                it
+            ) // BuildConfig.APPLICATION_ID: "org.techtown.seminar3"
+            // "org.techtown.seminar3.fileprovider"가 인증, 파일로부터 uri가져오기
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        changeProfileImage()
-        setContentView(binding.root)
+            // 3) 생성된 Uri를 Intent에 Put
+            fullSizeCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        }
+        return fullSizeCaptureIntent
     }
 
-    private fun saveImageFile(fileName: String, mimeType: String, bitmap: Bitmap): Uri? {
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
-        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            values.put(MediaStore.Images.Media.IS_PENDING, 1)
+    /**
+     * 1) 이미지 넣을 앱 내 파일 만들기
+     */
+    @Throws(IOException::class)
+    private fun createImageFile(storageDir: File?): File {
+        Log.d(TAG, "MainActivity - createImageFile() called")
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        // Pictures 디렉토리에 2022_****.jpeg형태로 빈파일 저장
+        return File.createTempFile(
+            "$timeStamp _", /* prefix */
+            ".jpeg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            Log.d(TAG, "Created File AbsolutePath : $absolutePath")
         }
-        val uri: Uri? =
-            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        try {
-            uri?.let {
-                var descriptor = contentResolver.openFileDescriptor(uri, "w")
-                descriptor?.let {
-                    val fos = FileOutputStream(descriptor.fileDescriptor)
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-                    fos.close()
-                    return uri
-                }
-            }
-        } catch (e: Exception) {
-            Log.e(TAG, "saveImageFile: ${e.localizedMessage}")
-        }
-        return null
-    }
-
-    fun newFileName(): String {
-        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
-        val fileName = sdf.format(System.currentTimeMillis()) // 파일 이름 중복을 막기위해 현재시간으로
-        return fileName
     }
 
     private fun changeProfileImage() {
-
+        Log.d(TAG, "MainActivity - changeProfileImage() called")
         binding.btnGalleryImg.setOnClickListener {
             galleyPermissionLauncher.launch(STORAGE_READ_PERMISSION)
         }
